18aacac3e461c9d87f443d746f5b44a33f31a9c2
김성훈 <shkim11997@gmail.com>
2021-11-24 11:11:29 +0900

    #24 feat: 타이머 테이블 생성

--- src/main/java/site/bbichul/models/Time.java
+++ src/main/java/site/bbichul/models/Time.java
@@ -1,4 +1,47 @@
 package site.bbichul.models;
 
-public class Time {
-}
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import site.bbichul.dto.TimeRequestDto;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity
+public class Time extends TimeStamped{
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private int year;
+
+    @Column(nullable = false)
+    private int month;
+
+    @Column(nullable = false)
+    private int day;
+
+    @Column(nullable = false)
+    private int weekday;
+
+    @Column(nullable = false)
+    private int study_time;
+
+    @Column(nullable = false)
+    private Long userId;
+
+//    public Time(TimeRequestDto timeRequestDto, Long userId) {
+//        // 로그인 되어있는  ID 저장
+//        this.userId = userId;
+//        this.year = timeRequestDto.getYear();
+//        this.month = timeRequestDto.getMonth();
+//        this.day = timeRequestDto.getDay();
+//        this.weekday = timeRequestDto.getWeekday();
+//        this.study_time = timeRequestDto.getStudy_time();
+
+    }
+
