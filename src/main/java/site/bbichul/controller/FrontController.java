0df61978a7766586a06ce4d43c309eb2586674a9
KyeongWoo <kasugano_@naver.com>
2021-11-26 10:58:29 +0900

    #27 fix : 콘트롤러 클래스명 수정

--- src/main/java/site/bbichul/controller/FrontController.java
+++ src/main/java/site/bbichul/controller/FrontController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class    FrontController {
+public class FrontController {
 
     @GetMapping("/calendars")
     public String moveCalendarPage(){
