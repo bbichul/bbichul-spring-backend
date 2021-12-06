2f36de5fa5ea316bcb8e21684bc21d662f66112f
minjae <l_0306@naver.com>
2021-12-06 18:56:45 +0900

    #79 배포:eb포트 수정

---
+++ src/main/java/site/bbichul/controller/HomeController.java
@@ -0,0 +1,15 @@
+package site.bbichul.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import site.bbichul.security.UserDetailsImpl;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "OK";
+    }
+}
