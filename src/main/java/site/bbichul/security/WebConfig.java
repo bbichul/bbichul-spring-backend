c725bf8b1c1073cee7cd8c30f69710712d1f700f
minjae <l_0306@naver.com>
2021-12-07 12:04:12 +0900

    #81 배포: webconfig 파일 어노테이션 추가

--- src/main/java/site/bbichul/security/WebConfig.java
+++ src/main/java/site/bbichul/security/WebConfig.java
@@ -1,8 +1,10 @@
 package site.bbichul.security;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
