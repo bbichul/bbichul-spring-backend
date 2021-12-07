c395872671332a5772a88fca80d59b83a0d82689
minjae <l_0306@naver.com>
2021-12-07 10:03:27 +0900

    #81 배포: CORS 에러 해결을 위해 webconfig파일 추가

---
+++ src/main/java/site/bbichul/security/WebConfig.java
@@ -0,0 +1,13 @@
+package site.bbichul.security;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:63342/");
+    }
+}
