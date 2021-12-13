de3410571e782ccaa2c187e7409b3c4036f10b12
minjae <l_0306@naver.com>
2021-12-13 10:57:23 +0900

    배포: 로드밸런서 수정

--- src/main/java/site/bbichul/controller/HomeController.java
+++ src/main/java/site/bbichul/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping("/health")
     public String home() {
         return "OK";
     }
