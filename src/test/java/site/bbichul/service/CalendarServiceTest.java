0af682b74b5cfdba59337bba09ca211bdb73d767
KyeongWoo <kasugano_@naver.com>
2021-12-16 16:04:38 +0900

    test : 캘린더 서비스 테스트 코드 업데이트

--- src/test/java/site/bbichul/service/CalendarServiceTest.java
+++ src/test/java/site/bbichul/service/CalendarServiceTest.java
@@ -1,14 +1,13 @@
 package site.bbichul.service;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import site.bbichul.dto.CalendarMemoResponseDto;
+import site.bbichul.models.Team;
 import site.bbichul.models.User;
 import site.bbichul.models.UserCalendar;
 import site.bbichul.repository.CalendarMemoRepository;
@@ -19,8 +18,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -35,35 +34,27 @@ class CalendarServiceTest {
     @Mock
     UserRepository userRepository;
 
-    @Test
-    @DisplayName("캘린더 서비스 생성")
-    void createCalendarService() {
-        CalendarService calendarService = new CalendarService(userRepository, userCalendarRepository, calendarMemoRepository);
 
-        assertNotNull(calendarService);
-    }
+    @InjectMocks
+    CalendarService calendarService;
 
 
     @Test
     @DisplayName("팀이 없는 유저가 캘린더를 가지고 있지 않은 경우")
     void getUserInfoWhenDontHaveCalendar() {
 
+        //given
         User user = new User();
 
         user.setUsername("나무");
         user.setId(1L);
 
-        CalendarService calendarService = new CalendarService(userRepository, userCalendarRepository, calendarMemoRepository);
-
         ArgumentCaptor<UserCalendar> captor = ArgumentCaptor.forClass(UserCalendar.class);
-
 
         List<UserCalendar> notFoundCalendar = new ArrayList<>();
 
-        //Mockito.lenient().
-        when(userRepository.findByUsername("나무")).thenReturn(Optional.of(user));
-        when(userCalendarRepository.findAllByUserId(1L)).thenReturn(notFoundCalendar);
-
+        given(userRepository.findByUsername("나무")).willReturn(Optional.of(user));
+        given(userCalendarRepository.findAllByUserId(1L)).willReturn(notFoundCalendar);
 
         //when
         List<UserCalendar> resultCalendarList = calendarService.getUserInfo(user.getUsername());
@@ -84,13 +75,56 @@ class CalendarServiceTest {
     @Test
     @DisplayName("팀에 아직 캘린더가 없는 경우")
     void getUserInfoNotFoundTeamCalendar() {
+        //given
+
+        User user = new User();
+
+        user.setUsername("나무");
+        user.setId(1L);
+
+        Team testTeam = new Team();
+        testTeam.setId(1L);
+        testTeam.setTeamname("버스타조");
+        user.setTeam(testTeam);
+
+        ArgumentCaptor<UserCalendar> captor = ArgumentCaptor.forClass(UserCalendar.class);
+
+        List<UserCalendar> haveDataCalendar = new ArrayList<>();
+        UserCalendar testCalendar = new UserCalendar();
+        haveDataCalendar.add(testCalendar);
+
+        List<UserCalendar> notFoundCalendar = new ArrayList<>();
+
+        given(userRepository.findByUsername("나무")).willReturn(Optional.of(user));
+        given(userCalendarRepository.findAllByUserId(1L)).willReturn(haveDataCalendar);
+        given(userCalendarRepository.findAllByTeamId(1L)).willReturn(notFoundCalendar);
+
+
+        //when
+        List<UserCalendar> resultCalendarList = calendarService.getUserInfo(user.getUsername());
+
+        //then
+        verify(userCalendarRepository, times(1))
+                .save(captor.capture());
+
+        UserCalendar resultT = captor.getValue();
+
+        assertEquals(resultT.getTeam().getId(), testTeam.getId());
+        assertEquals(resultT.getTeam().getTeamname(), testTeam.getTeamname());
+        assertEquals(resultT.getCalendarName(), "팀 버스타조의 캘린더 1");
 
     }
 
 
 
     @Test
-    void updateMemo() {
+    void updateMemoNotFoundMemo() {
+        //given
+
+        //when
+
+
+
     }
 
     @Test
