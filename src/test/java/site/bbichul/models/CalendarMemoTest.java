ac26f7d4c979d7aad97170767b521869c6f985de
minjae <l_0306@naver.com>
2021-12-17 09:38:31 +0900

    wip

---
+++ src/test/java/site/bbichul/models/CalendarMemoTest.java
@@ -0,0 +1,21 @@
+package site.bbichul.models;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+
+public class CalendarMemoTest {
+
+    @Nested
+    @DisplayName("CalendarMemo 객체 만들기")
+    class CreateCalendarMemo {
+        Long id;
+        String dateData;
+        String contents;
+
+        @BeforeEach
+        void setup() {
+
+        }
+    }
+}
