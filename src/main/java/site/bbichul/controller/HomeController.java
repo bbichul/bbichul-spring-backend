d52f689c4189e935e0b9cda87f15fea5a5f78db5
minjae <l_0306@naver.com>
2021-12-22 20:46:42 +0900

    #139 feat: slack에 알림 연동해 상태관리하기

--- src/main/java/site/bbichul/controller/HomeController.java
+++ src/main/java/site/bbichul/controller/HomeController.java
@@ -4,11 +4,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HomeController {
 
     @GetMapping("/health")
     public String home() {
         return "OK";
+    }
+
+    @GetMapping("/alarm")
+    public String test() {
+        log.error("Slack error alarm");
+        log.info("Slack log info");
+        log.debug("Slack log debug");
+        return "test";
     }
 }
